4ceb31dd9966a80d8bdd4c6797436ace86c9d920
Simon <simonwarui93@gmail.com>
2024-03-18 22:52:14 +0300

    assigment done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'simon'
+let welcomeMessage = 'Welcome to Flatbook, simon!'
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, SIMON!'
+let  shortGreeting = 'Welcome, s!'
