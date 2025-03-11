b62c6176acb46d1372e3ece23c47b281e8642642
Simon <simonwarui93@gmail.com>
2025-03-11 23:37:11 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "Flatbook"
+let welcomeMessage = "Welcome to Flatbook, !"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, !"
+let shortGreeting = "F Welcome, !"
